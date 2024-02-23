9d0ce35159b929fbc5788d10be4507f15e615441
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-23 09:57:42 -0300

    CREATE: new class 13 react

---
+++ 3_AVANÇANDO_EM_REACT/avancando/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import Car from './assets/Car.jpg';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="Fundo.jpg" alt="Fundo" />
+      </div>
+
+      {/* Imagem em src */}
+      <div>
+        <img src={Car} alt="Carro" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
