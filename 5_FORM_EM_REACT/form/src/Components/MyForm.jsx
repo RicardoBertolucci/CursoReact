342fc2de84201babdc552e47967bf5433c73412c
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-02-29 15:28:40 -0300

    NEW: new exercise 24 react

--- 5_FORM_EM_REACT/form/src/Components/MyForm.jsx
+++ 5_FORM_EM_REACT/form/src/Components/MyForm.jsx
@@ -1,15 +1,43 @@
 import styles from "./MyForm.module.css";
 
+import { useState } from "react";
+
 const MyForm = () => {
+  //3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    // console.log(e.target.value);
+    setName(e.target.value);
+  };
+
+  // console.log(email);
+
   return (
     <div>
-      {/* Criação de Form */}
+      {/*1 - Criação de Form */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite o seu nome" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite o seu nome"
+            onChange={handleName}
+          />
         </div>
-        <input type="submit" value="Enviar"/>
+        {/* 2 - Label envolvendo inpout */}
+        <label>
+          <span>E-mail</span>
+          <input
+            type="email"
+            name="email"
+            placeholder="Digite o seu e-mail"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Enviar" />
       </form>
     </div>
   );
