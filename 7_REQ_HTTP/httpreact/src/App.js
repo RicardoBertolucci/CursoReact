987ea7c5a0305933131cc4c037bbe273775b59ef
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-03-12 17:58:18 -0300

    NEW: class challenge 32 react

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -1,27 +1,38 @@
+//Style
 import './App.css';
 
+//Library
 import { useState, useEffect } from "react";
+
+//Components
+//4 - Custom hook
+import { useFetch } from './hooks/useFetch';
+
+
 
 const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
 
+  //4 - Custom hook
+  const { data: items, httpConfig, loading, error } = useFetch(url);//Redefini o valor de data como items
+
   const [name, setName] = useState([]);
   const [price, setPrice] = useState([]);
 
   //1 - Resgatando dados
-  useEffect(() => {
-    const response = async () => {
-      const res = await fetch(url);
+  // useEffect(() => {
+  //   const response = async () => {
+  //     const res = await fetch(url);
 
-      const data = await res.json();
+  //     const data = await res.json();
 
-      setProducts(data);
-    }
+  //     setProducts(data); 
+  //   }
 
-    response();
-  }, []);
+  //   response();
+  // }, []);
 
   // 2 - Adicionando produtos
   const handleSubmit = async (e) => {
@@ -31,23 +42,49 @@ function App() {
       name, price,
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(product),
-    });
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(product),
+    // });
+
+    // // 3 - carregamento dinâmico
+    // const addedProduct = await res.json();
+
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    //Como o valor retornado da API é o item adicionado, nó simplesmente pegamos o retorno 
+    //e guardamos em uma variável, que irá acrescentar o produto sem a necessidade de recarregar a página
+
+    //5 - refatorando POST
+
+    httpConfig(product, "POST");
+
+    setName("");
+    setPrice("");
   };
+
+  //8 - DESAFIO 6
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE");
+  }
 
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - R$: {product.price}</li>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!error && <ul>
+        {items && items.map((product) => (
+          <li key={product.id}>
+            {product.name} - R$: {product.price}
+            <button onClick={() => handleRemove(product.id)}>Excluir</button>  
+          </li>
         ))}
-      </ul>
+      </ul>}
       <div className="add-product">
         <form onSubmit={handleSubmit}>
           <label>
@@ -58,7 +95,9 @@ function App() {
             Preço:
             <input type="number" value={price} name="price" onChange={(e) => setPrice(e.target.value)} />
           </label>
-          <input type="submit" value="Criar Produto" />
+          {/* 7 - state de loading no POST */}
+          {loading && <input type="submit" disabled value="Aguarde" />}
+          {!loading && <input type="submit" value="Criar Produto" />}
         </form>
       </div>
     </div>
