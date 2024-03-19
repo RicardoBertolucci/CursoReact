32474a85af454052520d6308d73d8979e7ee7772
RicardoBertolucci <Ric.bertolucci@gmail.com>
2024-03-19 16:15:24 -0300

    NEW: class 34 react

--- 8_REACT_ROUTER/reactroute/src/App.js
+++ 8_REACT_ROUTER/reactroute/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 // 1 - config react router
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+//components
+import Navbar from "./components/Navbar";
+
 //pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Product from './pages/Product';
 
-//components
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -20,6 +22,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          {/* 4 - rota dinâmica */}
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </div>
